0e7f1aad4ffe0b7a6cca731b3dd384c7beb44b74
Mahfudz Ainur Rif'an <mfdsix.1nd0@gmail.com>
2022-10-31 14:32:52 +0700

    finish my post service

---
+++ posts/index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const { randomBytes } = require("crypto")
+
+const app = express()
+const port = 4000
+
+const posts = []
+
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex')
+    const {
+        title
+    } = req.body
+
+    posts.push({
+        id, title
+    })
+
+    res.status(201).send(posts[id])
+})
+
+app.listen(port, () => {
+    console.log(`POST API SERVICE ${port}`)
+})
