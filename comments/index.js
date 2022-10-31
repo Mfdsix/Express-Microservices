a3bbfd9283b72368629a3e6bc6ad07c2f4ad6bde
Mahfudz Ainur Rif'an <mfdsix.1nd0@gmail.com>
2022-10-31 15:21:10 +0700

    finish bus event

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const { randomBytes } = require("crypto")
 const cors = require("cors")
+const axios = require("axios")
 
 const app = express()
 app.use(bodyParser.json())
@@ -13,16 +14,23 @@ const comments = []
 app.get('/posts/:postId/comments', (req, res) => {
     res.send(comments.filter(({post_id}) => post_id == req.params.postId))
 })
-app.post('/posts/:postId/comments', (req, res) => {
+app.post('/posts/:postId/comments', async(req, res) => {
     const id = randomBytes(4).toString('hex')
     const {
         content
     } = req.body
 
-    comments.push({
+    const data = {
         post_id: req.params.postId,
         id,
         content
+    }
+
+    comments.push(data)
+
+    await axios.post("http://localhost:4005", {
+        type: "CommentCreated",
+        data: data
     })
 
     res.status(201).send(comments[comments.length-1])
--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const { randomBytes } = require("crypto")
 const cors = require("cors")
+const axios = require("axios")
 
 const app = express()
 app.use(bodyParser.json())
@@ -13,14 +14,20 @@ const posts = []
 app.get('/posts', (req, res) => {
     res.send(posts)
 })
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     const id = randomBytes(4).toString('hex')
     const {
         title
     } = req.body
 
-    posts.push({
+    const data = {
         id, title
+    }
+    posts.push(data)
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data
     })
 
     res.status(201).send(posts[posts.length-1])
