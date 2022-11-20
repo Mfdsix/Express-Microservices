18476a41bb3b17dfd3319dcb4d7ec4f0c557c5a1
Mahfudz Ainur Rif'an <mfdsix.1nd0@gmail.com>
2022-11-20 23:23:26 +0700

    create new client app

---
+++ client-new/src/App.js
@@ -0,0 +1,16 @@
+import PostList from "./components/PostList"
+import PostCreate from "./components/PostCreate"
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
+    </div>
+  );
+}
+
+export default App;
---
+++ client-new/src/components/CommentList.js
@@ -0,0 +1,38 @@
+import React
+// , { useState, useEffect }
+from "react";
+// import axios from "axios";
+
+const CommentList = ({ comments }) => {
+  // const [comments, setComments] = useState(postComments);
+
+  // const fetchData = async () => {
+  //   const res = await axios.get(
+  //     `http://localhost:4001/posts/${postId}/comments`
+  //   );
+
+  //   setComments(res.data);
+  // };
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  const renderedComments = comments.map((comment) => {
+    switch(comment.status){
+      case "approved":
+        return <li key={comment.id}>{comment.content}</li>;
+      case "rejected":
+        return <li key={comment.id}>
+          <i>This comment include rude words !!!</i>
+        </li>;
+      default:
+        return <li key={comment.id}>This comment is still waiting approval</li>;
+
+    }
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;
